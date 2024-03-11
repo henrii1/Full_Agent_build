12047966fe4d4fdbc9cafd135fb078832cc85d41
Emerald Henry <87782037+henrii1@users.noreply.github.com>
2024-03-11 14:02:26 +0000

    ollama setup files

---
+++ setup/Mistral_crewai_model.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Variables
+model_name="mistral"
+custom_model_name="crewai-mistral"
+
+# Get the base model
+ollama pull $model_name
+
+# Create the model file
+ollama create $custom_model_name -f ./MistralModelfile

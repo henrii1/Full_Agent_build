464a19addd54c858b3bac6f68bf5ea9422237e7e
Emerald Henry <87782037+henrii1@users.noreply.github.com>
2024-03-12 14:12:14 +0000

    updates

--- langchain/rag_tool.py
+++ langchain/rag_tool.py
@@ -12,15 +12,15 @@ from dotenv import load_dotenv, find_dotenv
 
 _ = load_dotenv(find_dotenv())  # reading local files
 
-openai_api_key = os.environ["OPENAI_API_KEY"]
+openai_api_key = os.getenv('OPENAI_API_KEY')
 
 
 class RagTool(BaseModel):
     query: str = Field(description = "This should be a search query")
 
 
-@tool("rag-tool", args_schema=RagTool, return_direct=True)
-def rag(query: str) -> str:
+@tool("rag-tool-one", args_schema=RagTool, return_direct=True)
+def rag_one(query: str) -> str:
     """A tool that retrieves contents that are semantically relevant to the input query from the provided document.
 
     Args:
@@ -41,9 +41,39 @@ def rag(query: str) -> str:
 
     vectordb = DocArrayInMemorySearch.from_documents(splits, embedding)
 
-    result = vectordb.similarity_search(query, k = 4 )
-    result_string = "".join(result)
+    results = vectordb.similarity_search(query, k = 4 )
+    result_string = "\n\n".join(str(result) for result in results)
 
     return result_string
 
 
+
+
+"""The second RAG tool"""
+
+@tool("rag-tool-two", args_schema=RagTool, return_direct=True)
+def rag_two(query: str) -> str:
+    """A tool that retrieves contents that are semantically relevant to the input query from the provided document.
+
+    Args:
+        query (str): input query from the user.
+
+    Returns:
+        str: top k amount of retrieved content from the uploaded document. content that are semantically similar to the input query.
+    """
+    pdf_file_path = _ #placehloder code from html, the path to pdf file
+    
+    loader = PyPDFLoader(pdf_file_path)
+    pages = loader.load_and_split()
+
+    text_splitter = RecursiveCharacterTextSplitter()
+    splits = text_splitter.split_documents(pages)
+
+    embedding = OpenAIEmbeddings(api_key = openai_api_key)
+
+    vectordb = DocArrayInMemorySearch.from_documents(splits, embedding)
+
+    results = vectordb.similarity_search(query, k = 4 )
+    result_string = "\n\n".join(str(result) for result in results)
+
+    return result_string

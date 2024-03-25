87aa13f43e3e95a03ea7ca570920a899009bc78f
Emerald33 <31729272+Emerald33@users.noreply.github.com>
2024-03-25 17:29:01 +0000

    page changes

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_page():
+    return render_template('home.html')
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
